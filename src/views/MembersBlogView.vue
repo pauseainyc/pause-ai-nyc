365d4e84edee7a8fc18f917b34cb67ba3b119b93
nielseni6 <nielseni6@students.rowan.edu>
2026-03-23 13:41:08 -0400

    Added Members Blog placeholder page

---
+++ src/views/MembersBlogView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="members-blog">
+    <div class="section">
+      <div class="section-heading">Members Blog</div>
+      <div class="section-content">
+        <p>Coming soon!</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+#members-blog {
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  text-align: left;
+  line-height: 1.6;
+  margin: 0 auto;
+}
+.section {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin-bottom: 16px;
+  .section-heading {
+    font-size: 24px;
+  }
+  .section-content {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    border-radius: 12px;
+    box-shadow: 0 0px 0px rgba(0, 0, 0, 0.1);
+    margin: 0 16px;
+  }
+}
+
+@media (max-width: 600px) {
+  .section {
+    .section-heading {
+      font-size: 18px;
+    }
+  }
+}
+</style>
