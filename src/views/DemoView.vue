c208cd0e8a4d2a72872c81b36904cb53eb467136
nielseni6 <nielseni6@students.rowan.edu>
2026-03-31 13:22:03 -0400

    fixed local network hosting

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -133,7 +133,8 @@ interface CharacterOut {
   level_descriptions: string[]
 }
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8000'
+const API_BASE =
+  import.meta.env.VITE_API_BASE || `${window.location.protocol}//${window.location.hostname}:8000`
 
 const characters = ref<CharacterOut[]>([])
 const selectedCharId = ref('')
